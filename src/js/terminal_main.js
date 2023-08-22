2c5272c158c0d82917d4ed5c720b6b4f5491e6e2
Administrator <baimanjvn666@outlook.com>
2023-08-22 09:35:05 +0800

    完善功能

--- src/js/terminal_main.js
+++ src/js/terminal_main.js
@@ -6,20 +6,21 @@ console.log = (logText) => {
 
 function loadCommands(cmd_name, source = "https://source.goindog.cn/terminal/") {
     let request = new XMLHttpRequest();
-    request.open("GET", source + cmd_name);
-    switch (request.status) {
-        case 200:
-            console.log("GET " + source + " SUCCESS!");
-            eval(request.responseText);
-            break;
-        default:
-            console.log("Bad Connection:" + request.status);
-    }
+    request.addEventListener("load",() => {
+        console.oldLog("GET " +  source + cmd_name + ".js" + " SUCCESS!");
+      
+    })
+    request.open("GET", source + cmd_name + ".js");
+    request.send();
 }
 
 function runCommand(commandName = "", Attribute = []) {
     let command_run_str = `${commandName}(${JSON.stringify(Attribute)})`
-    eval(command_run_str);
+    try {
+        eval(command_run_str);
+    } catch (e) {
+        console.log("Unknow command:" + commandName);
+    }
     document.getElementById("code_input").setAttribute("id", "code_ip_");
     document.getElementById("ip_cmd").setAttribute("id", "ip_cmd_");
     document.getElementById("br_div").setAttribute("id", "br_div_");
@@ -49,46 +50,6 @@ window.onload = () => {
             } else {
                 document.getElementById("ip_cmd").innerText += ev.key
             }
-    })
-}
-
-function Http(param) {
-    let attr = JSON.parse("{}");
-    for (let i = 0; i < param.length; i++) {
-        if (param[i].substring(0, 1) === "-") {
-            attr[param[i].substring(1, param[i].length)] = param[i + 1];
-        }
-    }
-    let http = new XMLHttpRequest();
-    let url = attr["type"] + "://" + attr["url"] + ":" + attr["port"];
-    let meth = attr["method"];
-    http.open(meth, url);
-    http.send();
-    http.onload = (ev) => {
-        console.log(http.response);
-        return http.response;
-    }
-}
-function term(param = []) {
-    let attrib = JSON.parse("{}");
-    for (let i = 0; i < param.length; i++) {
-        if (param[i].substring(0, 1) === "-") {
-            attrib[param[i].substring(1, param[i].length)] = param[i + 1];
-        }
-    }
-    if (attrib["add"] !== undefined) {
-        loadCommands(attrib["-n"]);
-    }
-    if (attrib["color"] !== undefined) {
-        document.body.innerHTML += `
-        <style>
-        code {
-        color: ${attrib["-new"]} !important;
-        }
-        #mouse {
-        background-color: ${attrib["-new"]} !important;
-        }
-</style>
-        `
-    }
+    });
+    loadCommands("common");
 }
